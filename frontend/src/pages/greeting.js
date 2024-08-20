7033d56852eca8a2d174955780773a6304d7d728
Tobi Adeleke <tobiaadeleke@gmail.com>
2024-08-20 23:19:46 +0100

    add hello from API endpoint

--- frontend/src/pages/greeting.js
+++ frontend/src/pages/greeting.js
@@ -1,24 +1,29 @@
 import React, { useEffect, useState} from 'react';
 
 export default function HelloWorld(){
-    const [message, setMessage] = useState('');
+    const [message, setMessage] = useState(null);
+
     useEffect(() => {
         async function greetData(){
             const response = await fetch('http://localhost:8000/api/hello-world/');
-            console.log(response);
+            console.log("Request sent");
             if(!response.ok){
-                console.log(response);
-                throw new Error("Could not fetch Postcode");
+                throw new Error("Could not fetch Welcome");
             }
-        
-            setMessage(response.json().message); //TODO: Stringify
+            const data = await response.json();
+            setMessage(data);
+            console.log(data);
         }
         greetData();
     },[]);
+
+    if (!message){
+        return <div>Loading...</div>;
+    }
     return(
         <div>
             <h1> Hi from React  </h1>
-            <p>{message}</p>
+            <p>{message.message}</p>
         </div>
     )
 }
