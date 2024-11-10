89709e03da80f1238151d58c8fe510a9e8c7a376
Tobi Adeleke <tobiaadeleke@gmail.com>
2024-11-10 18:52:30 +0000

    update validation in serailizer to include decode

--- backend/csv_to_json/serializers.py
+++ backend/csv_to_json/serializers.py
@@ -8,12 +8,6 @@ class CSVUploadSerializer(serializers.ModelSerializer):
         model = CSVUpload
         fields = ['file']
 
-    def create(self, validated_data):
-        """To allow the serializer.save()
-        to return an instance of the validated data.
-        """
-        return super().create(validated_data)
-
     def validate(self, attrs):
         """
         To validate the file extension, size and csv readability
@@ -34,11 +28,12 @@ class CSVUploadSerializer(serializers.ModelSerializer):
         try:
             file.open()
             # Read a small portion to validate
-            csv.Sniffer().sniff(file.read(1024))
+            csv.Sniffer().sniff(file.read(1024).decode('utf-8'))
             # Reset file pointer after reading
             file.seek(0)
-        except Exception:
+        except Exception as e:
+            print(e)
             raise serializers.ValidationError(
                 'The uploaded file is not a readable CSV'
                 )
-        return super().validate(attrs)
+        return attrs
