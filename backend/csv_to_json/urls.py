014fe8a24428ef5635639eb880748b61c60ffe6b
Tobi Adeleke <tobiaadeleke@gmail.com>
2025-01-04 20:35:51 +0000

    add JSONDownloadView endpoints and some formatting

--- backend/csv_to_json/urls.py
+++ backend/csv_to_json/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import hello_world, CSVUploadView
+from .views import (
+    hello_world,
+    CSVUploadView,
+    JSONDownloadView,
+    EmailSendView
+)
 
 urlpatterns = [
     path('hello-world/', hello_world, name='hello_world'),
     path('upload/', CSVUploadView.as_view(), name='csv-upload'),
+    path('download/<int:pk>/',
+         JSONDownloadView.as_view(),
+         name='json-download'),
+    path('email/', EmailSendView.as_view(), name='email-send'),
 ]
