c3b6b5c54c0baf08b1c175e8cbca246194fd9c5c
Tobi Adeleke <tobiaadeleke@gmail.com>
2025-01-04 20:34:18 +0000

    update JSONDownloadView class to return Json data and rate limits

--- backend/csv_to_json/views.py
+++ backend/csv_to_json/views.py
@@ -1,10 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.throttling import AnonRateThrottle
 from rest_framework import status
+from django.conf import settings
+from django.http import FileResponse
 from .serializers import CSVUploadSerializer
+from .models import CSVUpload
 from .task import process_csv
 from .util import get_filefield_name
+import os
 
 
 @api_view(['GET'])
@@ -31,9 +36,48 @@ class CSVUploadView(APIView):
 
             return Response(
                     {"message":
-                     f"File uploaded successfully {formatted_filename}."},
+                     f"File uploaded successfully {formatted_filename}.",
+                     "fileID":
+                     file_instance.id},
                     status=status.HTTP_201_CREATED)
 
         return Response(
-                {"message": serializer.errors},
+                {"message": serializer.errors,
+                 "fileID": None},
                 status=status.HTTP_400_BAD_REQUEST)
+
+
+class JSONDownloadView(APIView):
+    throttle_classes = [AnonRateThrottle]
+
+    def get(self, request, pk):
+        try:
+            file_instance = CSVUpload.objects.get(pk=pk)
+        except Exception as err:
+            print(err)
+            return Response({'error': 'File not found'},
+                            status=status.HTTP_404_NOT_FOUND)
+
+        # Get Json filepath
+        json_filepath = os.path.join(settings.MEDIA_ROOT, file_instance.result_url)
+
+        if not os.path.exists(json_filepath):
+            return Response({'error': 'File not found'},
+                            status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            formatted_filename = get_filefield_name(file_instance.file.name)
+
+            response = FileResponse(open(json_filepath, 'rb'),
+                                    as_attachment=True,
+                                    filename=f'{formatted_filename}.json')
+
+            return response
+        except Exception as err:
+            print(err)
+            return Response({'error': 'Error Loading File'},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class EmailSendView(APIView):
+    pass
